cd6f9edeeedb0a5fe88499e61fd96e5884c653dc
Chidubem Uchendu <dubemxe@gmail.com>
2024-02-04 13:50:42 -0800

    code Update

--- api/v1/app.py
+++ api/v1/app.py
@@ -14,3 +14,13 @@ app.register_blueprint(app_views)
 def close_db(error):
     """ Close Storage """
     storage.close()
+
+if __name__ == "__main__":
+    """ Main Function """
+    host = environ.get('HBNB_API_HOST')
+    port = environ.get('HBNB_API_PORT')
+    if not host:
+        host = '0.0.0.0'
+    if not port:
+        port = '5000'
+    app.run(host=host, port=port, threaded=True)
