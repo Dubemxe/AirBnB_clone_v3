33df7e2b767c244780dd7779a90b5e5b21f7d56a
Chidubem Uchendu <dubemxe@gmail.com>
2024-02-04 23:54:44 -0800

    Code update

--- api/v1/app.py
+++ api/v1/app.py
@@ -20,12 +20,18 @@ def close_db(exception):
     storage.close()
 
 
+@app.errorhandler(404)
+def not_found(error):
+    """404 Error
+    Description: a resource was not found
+    """
+    return make_response(jsonify({'error': "Not found"}), 404)
+
+
 if __name__ == "__main__":
     """Main Function"""
-    host = getenv('HBNB_API_HOST')
-    port = getenv('HBNB_API_PORT')
-    if not host:
-        host = '0.0.0.0'
-    if not port:
-        port = '5000'
+
+    host = getenv('HBNB_API_HOST', '0.0.0.0')
+    port = int(getenv('HBNB_API_PORT', '5000'))
+
     app.run(host=host, port=port, threaded=True)
