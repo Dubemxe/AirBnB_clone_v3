f76621f4654503bddcb651ece644d11957091f2a
Chidubem Uchendu <dubemxe@gmail.com>
2024-02-04 23:44:00 -0800

    Code update

--- api/v1/app.py
+++ api/v1/app.py
@@ -3,23 +3,27 @@
 Start Api
 '''
 from models import storage
-from os import environ
+from os import getenv
+from flask_cors import CORS
 from api.v1.views import app_views
 from flask import Flask
+
+
 app = Flask(__name__)
-
-
 app.register_blueprint(app_views)
+cors = CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
+
 
 @app.teardown_appcontext
-def close_db(error):
+def close_db(exception):
     """Close Storage"""
     storage.close()
 
+
 if __name__ == "__main__":
     """Main Function"""
-    host = environ.get('HBNB_API_HOST')
-    port = environ.get('HBNB_API_PORT')
+    host = getenv('HBNB_API_HOST')
+    port = getenv('HBNB_API_PORT')
     if not host:
         host = '0.0.0.0'
     if not port:
